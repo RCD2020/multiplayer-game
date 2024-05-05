415283e59d718340e75ef932102198548af64033
RCD2020 <dobertravis@icloud.com>
2024-05-05 13:20:23 -0600

    tested out way to identify clients

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ Robert Davis
 2024.05.04
 '''
 
-from flask import Flask, render_template, url_for
+from server.ServerInstance import ServerInstance
+
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-# TODO create server instance
+server = ServerInstance()
 
 
 @app.route('/')
@@ -26,7 +28,16 @@ def handle_send_server(json):
     socketio.emit('send_client', json)
 
 
+@socketio.on('connect')
+def handle_connect():
+    # this can be used as a unique identifier for clients
+    print('sid', request.sid)
+
+
 # TODO handle sockets between server and client
+@socketio.on('server_data')
+def handle_data(data: dict):
+    socketio.emit('client_data', data)
 
 
 if __name__ == '__main__':
