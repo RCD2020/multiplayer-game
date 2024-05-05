5e9ace253fff1022aaaae1ae7e379aec562130d7
RCD2020 <dobertravis@icloud.com>
2024-05-05 13:37:10 -0600

    rearranged and grouped app.py

--- app.py
+++ app.py
@@ -13,21 +13,17 @@ socketio = SocketIO(app)
 server = ServerInstance()
 
 
-@app.route('test/')
-def test_landing():
-    return render_template('index.html')
+# ----------------------------------------------------------------------
+# PRODUCTION LANDINGS
+# ----------------------------------------------------------------------
+@app.route('/')
+def join_game():
+    return render_template('game_id.html')
 
 
-@socketio.on('send_json')
-def handle_send_json():
-    emit('json_response', {'test': True})
-
-
-@socketio.on('send_server')
-def handle_send_server(json):
-    socketio.emit('send_client', json)
-
-
+# ----------------------------------------------------------------------
+# PRODUCTION SOCKETS
+# ----------------------------------------------------------------------
 @socketio.on('connect')
 def handle_connect():
     # this can be used as a unique identifier for clients
@@ -38,6 +34,24 @@ def handle_connect():
 @socketio.on('server_data')
 def handle_data(data: dict):
     socketio.emit('client_data', data)
+
+
+# ----------------------------------------------------------------------
+# TEST
+# ----------------------------------------------------------------------
+@app.route('/test')
+def test_landing():
+    return render_template('test_landing.html')
+
+
+@socketio.on('send_json')
+def handle_send_json():
+    emit('json_response', {'test': True})
+
+
+@socketio.on('send_server')
+def handle_send_server(json):
+    socketio.emit('send_client', json)
 
 
 if __name__ == '__main__':
