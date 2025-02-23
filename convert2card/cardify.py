e2bc7ecaefce06bc354fc07309247f0c6470278f
RCD2020 <dobertravis@icloud.com>
2025-02-22 18:27:05 -0700

    will now generate a rgb and oklabs version for each image listed in IMAGES

--- convert2card/cardify.py
+++ convert2card/cardify.py
@@ -5,31 +5,42 @@ from oklab import rgb2oklab
 from card_colors import color_palette
 
 
-ISOKLABS = True
+# ISOKLABS = True
 WIDTH = 216
 HEIGHT = 302
-IMG_NAME = 'green.jpg'
-IMG_URL = 'convert2card/images/input/' + IMG_NAME
+IMAGES = [
+    'candlestick.jpg',
+    'knife.jpg',
+    'lead pipe.jpg',
+    'revolver.jpg',
+    'rope.png',
+    'wrench.png'
+]
 
 
-im = Image.open(IMG_URL)
-im = im.resize((WIDTH, HEIGHT))
-pixels = im.load()
-values = color_palette()
-tree = spatial.KDTree(color_palette(ISOKLABS))
+for img_name in IMAGES:
+    for isoklabs in [True, False]:
+        IMG_URL = 'convert2card/images/input/' + img_name
 
 
-for i in range(im.size[0]):
-    for j in range(im.size[1]):
-        if ISOKLABS:
-            _, k = tree.query(rgb2oklab(*pixels[i,j][0:3]))
-        else:
-            _, k = tree.query(pixels[i,j][0:3])
-        
-        pixels[i, j] = values[k]
+        im = Image.open(IMG_URL)
+        im = im.resize((WIDTH, HEIGHT))
+        pixels = im.load()
+        values = color_palette()
+        tree = spatial.KDTree(color_palette(isoklabs))
 
-img_type = 'rgb'
-if ISOKLABS:
-    img_type = 'OKlabs'
 
-im.save(f'convert2card/images/output/{IMG_NAME.split(".")[0]}_{img_type}.png')
+        for i in range(im.size[0]):
+            for j in range(im.size[1]):
+                if isoklabs:
+                    _, k = tree.query(rgb2oklab(*pixels[i,j][0:3]))
+                else:
+                    _, k = tree.query(pixels[i,j][0:3])
+                
+                pixels[i, j] = values[k]
+
+        img_type = 'rgb'
+        if isoklabs:
+            img_type = 'OKlabs'
+
+        im.save(f'convert2card/images/output/{img_name.split(".")[0]}_{img_type}.png')
